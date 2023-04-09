1831ff8ed6c89a960d2e5cf0bfe8fbaec7817400
sumit kumar <sumokhan76342@gmail.com>
2023-04-09 21:03:54 +0530

    correction

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-mongoose.set("strictQuery",true)
+mongoose.set("strictQuery", true)
 mongoose.connect('mongodb+srv://TusharJainFunctionup:functionup@tusharjaindb.zxey2fj.mongodb.net/PostApp')
-.then(()=> console.log("Db connected successfully"))
-.catch((err)=> console.log(err))
+    .then(() => console.log("Db connected successfully"))
+    .catch((err) => console.log(err))
 
-app.use('/',route)
+app.use('/', route)
 
 let port = process.env.PORT || 3001;
 
-app.listen(port, ()=> {
-    console.log(`Port is running on ${port}`)
-})
+app.listen(port, () => { console.log(`Port is running on ${port}`) })
